20e514b6a2cbab848144196e32c3b08d7b016ea5
Kayo Riccelo <kayo.riccelo@hotmail.com>
2019-10-20 19:23:18 -0300

    Criado componente Message;

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
 import paginationReducer from '../components/List/Pagination/store/ducks';
+import messageReducer from '../components/Layout/Message/store/ducks';
 
 import signInReducer from '../pages/SignIn/store/ducks';
 import signUpReducer from '../pages/SignUp/store/ducks';
@@ -11,6 +12,7 @@ const reducers = combineReducers({
     router: routerReducer,
 
     pagination: paginationReducer,
+    message: messageReducer,
 
     signin: signInReducer,
     signup: signUpReducer,
