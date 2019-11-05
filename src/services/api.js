1495d0b018b95ff2e7b25d9c831c48ee390b25f0
Kayo Riccelo <kayo.riccelo@hotmail.com>
2019-11-04 23:39:59 -0300

    Alterando rota api para heroku;

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 // import { rota } from '../env';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000'
+    baseURL: 'https://apicontrolerotas.herokuapp.com'
 })
 
 api.interceptors.request.use(config => {
@@ -44,7 +44,7 @@ api.interceptors.response.use(
 );
 
 export const apiNotToken = axios.create({
-    baseURL: 'http://127.0.0.1:8000'
+    baseURL: 'https://apicontrolerotas.herokuapp.com'
 });
 
 export default api;
