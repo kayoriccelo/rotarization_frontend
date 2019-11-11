5a489bf3b767c74ea68446464d13d64fdb3d98a3
Kayo Riccelo <kayo.riccelo@hotmail.com>
2019-11-11 00:02:52 -0300

    Ajuste visual padding inputs;

--- src/components/Inputs/Date/styles/index.js
+++ src/components/Inputs/Date/styles/index.js
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles({
     grid: {
-        padding: '0px 8px'
+        padding: '0px 4px'
     },
     input: {
         width: '100%'
--- src/components/Inputs/Time/styles/index.js
+++ src/components/Inputs/Time/styles/index.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles({
     grid: {
-        padding: '0px 8px'
+        padding: '0px 4px'
     },
     input: {
         width: '100%'
