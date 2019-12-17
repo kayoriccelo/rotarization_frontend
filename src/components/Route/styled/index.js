6acc2e81e59c3b74daae85b14d67d9f72c04db6e
Kayo Riccelo <kayo.riccelo@hotmail.com>
2019-12-17 13:47:39 -0300

    Adicionando animação nos componentes renderizado no content;

--- src/components/Route/styled/index.js
+++ src/components/Route/styled/index.js
@@ -1,12 +1,29 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 import commonStyles from '../../../commons/styles';
 
+
+const keyframeRoot = keyframes`
+    0% {
+        transform: translateX(1000px) scaleX(2.5) scaleY(0.2);
+        transform-origin: 0% 50%;
+        filter: blur(40px);
+        opacity: 0;
+    },
+    100% {
+        transform: translateX(0) scaleY(1) scaleX(1);
+        transform-origin: 50% 50%;
+        filter: blur(0);
+        opacity: 1;
+    }
+`;
 
 export const StyledRoot = styled.div`
     background: ${commonStyles.backgroundColor};
     height: 200px;
     width: 100%;
+    -webkit-animation: ${keyframeRoot} 0.6s cubic-bezier(0.230, 1.000, 0.320, 1.000) both;
+    animation: ${keyframeRoot} 0.6s cubic-bezier(0.230, 1.000, 0.320, 1.000) both;
 `;
 
 export const StyledMain = styled.div`
